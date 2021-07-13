1890da9538d4f85f3add83125a9edc129cc1abcb
Matt Dillemuth <mattdillemuth@gmail.com>
2021-07-12 23:43:08 -0400

    connect to mongoDB

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
 const express = require('express')
 const mongo = require('mongodb').MongoClient
+const url = 'mongodb://localhost:27017'
 
 // Init Express
 const app = express()
+app.use(express.json())
+
+// Init MongoDB
+mongo.connect(
+  url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    db = client.db('tripcost')
+    trips = db.collection('trips')
+    expenses = db.collection('expenses')
+  }
+)
 
 // API Endpoints
 app.post('/trip', (req, res) => {
   /* */
 })
+
 app.get('/trips', (req, res) => {
   /* */
 })
